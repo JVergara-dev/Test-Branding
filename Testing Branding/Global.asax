fbcfeb9653980e1f890766007c72ec08512813b6
Jonas Vergara <vergara.jonas.g.git@gmail.com>
2021-04-27 20:25:35 -0500

    Add project files.

---
+++ Testing Branding/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Testing_Branding.MvcApplication" Language="C#" %>
---
+++ Testing Branding/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Testing_Branding.Startup))]
+namespace Testing_Branding
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
